9dd1ff1bd4a4e5b991ecbf77e50d3685401d961e
Jesdin Raphael <jesdinraphael@gmail.com>
2018-11-17 10:51:56 +0530

    ui update
    added completed option in TaskCard and added ajax function to send put request on clicking the checkbox
    also removed the completed option in edit page

--- TaskManagerApp/assets/bundle.js
+++ TaskManagerApp/assets/bundle.js
@@ -1,4 +1,4 @@
 import './scss/site.scss'
 
-import '../node_modules/jquery/dist/jquery.slim.js'
+import '../node_modules/jquery/dist/jquery.js'
 import './../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
