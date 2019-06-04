44c7fa3453fc2136b7adf5da356629235002ee0c
Jesdin Raphael <jesdinraphael@gmail.com>
2019-06-04 12:13:52 +0530

    update description box for input

--- TaskManagerApp/Models/MyTask.cs
+++ TaskManagerApp/Models/MyTask.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace TaskManagerApp.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
         public bool IsCompleted { get; set; }
         public bool IsHighPriority { get; set; }
